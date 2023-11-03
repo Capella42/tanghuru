aa6fa16772b6e9f5fa83e3de50f16b1373c6ff7b
Capella42 <83754482+Capella42@users.noreply.github.com>
2023-11-03 17:36:42 +0900

    1103

---
+++ settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1882343.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 154508448, 154511088, 154511090, 154511091, 154511093, 154511926, 154512167, 154522462, 154743574, 154750601, 154960921, 155038835, 155740621, 155761635, 155882863 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
